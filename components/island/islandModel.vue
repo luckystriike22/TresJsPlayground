500a22efa27422ddef0a7d4d6928c8070a001678
Luckystriike <glenny@me.com>
2024-05-27 14:09:21 +0200

    grass and farmer

--- components/island/islandModel.vue
+++ components/island/islandModel.vue
@@ -1,9 +1,11 @@
 <template>
-    <FBXModel ref="model" path="/islandAssets/models/Island.fbx" :scale="0.03 * $props.islandSizeFactor" :position="[-0.9,0.8,6]" cast-shadow receive-shadow :rotation="[0,1.3,0]"/>
+     <primitive :object="nodes.Island__1_" :scale="0.03 * $props.islandSizeFactor" :position="[-5,0.8,6]" cast-shadow receive-shadow :rotation="[1.6,0,-1]" />
 </template>
 
 <script lang="ts" setup>
 const args = defineProps(["islandSizeFactor"])
+const { nodes } = await useGLTF('/islandAssets/models/Island.glb', { draco: true });
+console.log(nodes)
 </script>
 
 <style scoped></style>
